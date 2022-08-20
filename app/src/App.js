b396cfb0d1426d3fe36f596508998ca3d31663bb
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-08-20 23:34:13 +0530

    Added Online Video Player

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactPlayer from 'react-player'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ReactPlayer controls url= 'https://youtu.be/0e3GPea1Tyg' />
     </div>
   );
 }
