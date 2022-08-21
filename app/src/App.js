6b6e0e09f433e204c6c07b311e7e6a3fd3e914fc
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-08-21 17:41:07 +0530

    Configured for Tailwind

--- app/src/App.js
+++ app/src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
+    <h1>Curio CIC</h1>
     <ReactPlayer controls url= 'https://youtu.be/0e3GPea1Tyg' />
     </div>
   );
