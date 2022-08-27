fc53bacec84491b923b3f322db62328b24fbfd66
Arjxn.py <arjxn.py@gmail.com>
2022-08-28 05:25:38 +0530

    Recorder UI Added (#4)
    * Recorder UI Added
    
    * Update curio/app/src/components/App.js
    
    Co-authored-by: Saransh <saransh0701@gmail.com>
    
    Co-authored-by: Saransh <saransh0701@gmail.com>

--- curio/app/src/components/App.js
+++ curio/app/src/components/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-// import { Recorder } from 'react-voice-recorder';
+// import { Recorder , myRecorder} from 'react-voice-recorder';
+import {button} from './recorder'
 // import ReactPlayer from 'react-player/youtube'
 import './App.css'
 
 function App () {
   return(
-    <h1>Hello World</h1>
+    <div>
+    {button()}
+    </div>
+    
   )
-  // render(){
-    // (
-    // <div className="App">
-    // <h1 class = "text-gray-700 font-bold bg-amber-400">Curio CIC</h1>
-    // {/* <ReactPlayer controls url= 'https://youtu.be/0e3GPea1Tyg' /> */}
-    // </div>
-    // <Recorder />
-  // );
-    // return Recorder
-// }
 }
 
 export default App;
--- curio/app/src/components/recorder.js
+++ curio/app/src/components/recorder.js
@@ -1,11 +1,34 @@
-
-import {Recorder} from 'react-voice-recorder'
+// import {Recorder} from 'react-voice-recorder'
 import 'react-voice-recorder/dist/index.css'
 
-function myRecorder() {
+export function button() {
     return (
-      Recorder
+    <>
+      <form class="w-full max-w-sm">
+        <div class="md:flex md:items-center mb-6">
+          <div class="md:w-1/3">
+            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+              Enter Duration <br/> In Seconds
+            </label>
+          </div>
+          <div class="md:w-2/3">
+            <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="number" min={0} max={60} placeholder='1-60'/>
+          </div>
+        </div>
+
+        <div class="md:flex md:items-center">
+          <div class="md:w-1/3"></div>
+          <div class="md:w-2/3">
+            <button class="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="button">
+              Record
+            </button>
+          </div>
+        </div>
+      </form>
+    </>
     );
   }
+
+
   
-  export default myRecorder;
+  // export default myRecorder;
