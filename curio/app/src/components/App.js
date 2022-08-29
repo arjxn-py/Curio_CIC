e23db7794634a102c9e1a654502f25a9dc777eb6
Arjxn.py <arjxn.py@gmail.com>
2022-08-29 23:25:15 +0530

    Added Recorder Function Alongwith Speech-to-text (#5)
    * Recorder UI Added
    
    * Update curio/app/src/components/App.js
    
    Co-authored-by: Saransh <saransh0701@gmail.com>
    
    * Added Recorder Function Alongwith Speech-to-text
    
    * Player UI Added
    
    Co-authored-by: Saransh <saransh0701@gmail.com>

--- curio/app/src/components/App.js
+++ curio/app/src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import { Recorder , myRecorder} from 'react-voice-recorder';
-import {button} from './recorder'
 // import ReactPlayer from 'react-player/youtube'
+import {Form} from './recorder'
 import './App.css'
 
 function App () {
   return(
     <div>
-    {button()}
+    {Form()}
     </div>
     
   )
--- curio/app/src/components/recorder.js
+++ curio/app/src/components/recorder.js
@@ -1,34 +1,91 @@
+import React , {useState,useEffect} from 'react';
 // import {Recorder} from 'react-voice-recorder'
-import 'react-voice-recorder/dist/index.css'
+// import 'react-voice-recorder/dist/index.css'
 
-export function button() {
-    return (
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition
+const mic = new SpeechRecognition()
+
+mic.continuous = true
+mic.interimResults = true
+mic.lang = 'en-US'
+
+export const Form = () => {
+
+  const [isListening , setIsListening] = useState(false)
+
+  useEffect(()=> {
+    handleListen()
+  }, [isListening])
+
+  
+  const handleListen = () => {
+    if(isListening){
+      mic.start()
+      mic.onend = () => {
+        console.log('Continue...');
+        mic.start()
+      }
+    }else {
+      mic.stop()
+      mic.onend = () => {
+        console.log('Stopped Mic On Click');
+      }
+    }
+  }
+
+  mic.onstart = () => {
+    console.log('Mics on')
+  }
+
+  mic.onresult = event => {
+    const transcript = Array.from(event.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('')
+    console.log(transcript)
+    mic.onerror = event => {
+      console.log(event.error)
+    }
+  }
+
+
+  return (
     <>
-      <form class="w-full max-w-sm">
-        <div class="md:flex md:items-center mb-6">
-          <div class="md:w-1/3">
-            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+      <form className="w-full max-w-sm">
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
               Enter Duration <br/> In Seconds
             </label>
           </div>
-          <div class="md:w-2/3">
-            <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="number" min={0} max={60} placeholder='1-60'/>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="number" min={0} max={60} placeholder='1-60'/>
           </div>
         </div>
 
-        <div class="md:flex md:items-center">
-          <div class="md:w-1/3"></div>
-          <div class="md:w-2/3">
-            <button class="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="button">
+        {isListening ? <span>🎙️  Recording</span> : <span>🛑🎙️ Currently Not Recording</span>}
+
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3">
+            <button id = "record" onClick={() => {setIsListening(prevState => !prevState)}} className="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="button">
               Record
             </button>
           </div>
         </div>
       </form>
+
+    <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3">
+            <audio controls className="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="audio">
+            </audio>
+          </div>
+    </div>
+
+    
     </>
-    );
-  }
 
-
-  
-  // export default myRecorder;
+  );
+}
