4e7297fd8b0f32d81b737343eac2d0ec8ae7a3a4
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-08-30 13:57:31 +0530

    Added Functional Components For Recorder

--- curio/app/src/components/recorder.js
+++ curio/app/src/components/recorder.js
@@ -1,91 +1,111 @@
-import React , {useState,useEffect} from 'react';
-// import {Recorder} from 'react-voice-recorder'
-// import 'react-voice-recorder/dist/index.css'
+import React , {Component} from 'react';
+import MicRecorder from 'mic-recorder-to-mp3';
 
-const SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition
-const mic = new SpeechRecognition()
-
-mic.continuous = true
-mic.interimResults = true
-mic.lang = 'en-US'
-
-export const Form = () => {
-
-  const [isListening , setIsListening] = useState(false)
-
-  useEffect(()=> {
-    handleListen()
-  }, [isListening])
-
-  
-  const handleListen = () => {
-    if(isListening){
-      mic.start()
-      mic.onend = () => {
-        console.log('Continue...');
-        mic.start()
+const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+export default class Audio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        isRecording: false,
+        blobURL: '',
+        isBlocked: false,
+        isRecordingStp: false,
       }
-    }else {
-      mic.stop()
-      mic.onend = () => {
-        console.log('Stopped Mic On Click');
-      }
+
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+    this.reset = this.reset.bind(this);
+   }
+
+  componentDidMount(){
+    navigator.getUserMedia = (
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia
+     );
+
+     navigator.getUserMedia({ audio: true },
+      () => {
+        console.log('Permission Granted');
+        this.setState({ isBlocked: false });
+      },
+      () => {
+        console.log('Permission Denied');
+        this.setState({ isBlocked: true })
+      },
+    );
+  }
+ 
+  start(){
+    if (this.state.isBlocked) {
+      alert('Permission Denied');
+    } else {
+      Mp3Recorder
+        .start()
+        .then(() => {
+          this.setState({ isRecording: true });
+        }).catch((e) => console.error(e));
     }
   }
 
-  mic.onstart = () => {
-    console.log('Mics on')
+  stop() {
+    Mp3Recorder
+      .stop()
+      .getMp3()
+      .then(([buffer, blob]) => {
+        const blobURL = URL.createObjectURL(blob)
+        this.setState({ blobURL, isRecording: false });
+        this.setState({ isRecordingStp: true });
+      }).catch((e) => console.log(e));
+  };
+
+  reset() {
+      document.getElementsByTagName('audio')[0].src = '';
+      this.setState({ isRecordingStp: false });
+  };
+
+  render() {
+
+    return(
+      <>
+       <form className="w-full max-w-sm">
+
+         <div className="md:flex md:items-center mb-6">
+           <div className="md:w-1/3">
+             <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
+               Enter Duration <br/> In Seconds
+             </label>
+           </div>
+           <div className="md:w-2/3">
+             <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="number" min={0} max={60} placeholder='1-60'/>
+           </div>
+         </div>
+
+         <div className="md:flex md:items-center">
+           <div className="md:w-1/3"></div>
+           <div className="md:w-2/3">
+             <button id = "record" onClick={this.start} disabled={this.state.isRecording} className="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="button">
+               Record
+             </button>
+             <button id = "stop" onClick={this.stop} disabled={!this.state.isRecording} className="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="button">
+               Stop
+             </button>
+           </div>
+         </div>
+
+       </form>
+
+     <div className="md:flex md:items-center">
+        <div className="md:w-1/3">
+          <div className="md:w-2/3">
+           <audio src={this.state.blobURL} controls className="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="audio">
+             </audio>
+          </div>
+       </div>
+      </div>
+
+     </>
+    );
   }
-
-  mic.onresult = event => {
-    const transcript = Array.from(event.results)
-      .map(result => result[0])
-      .map(result => result.transcript)
-      .join('')
-    console.log(transcript)
-    mic.onerror = event => {
-      console.log(event.error)
-    }
-  }
-
-
-  return (
-    <>
-      <form className="w-full max-w-sm">
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
-              Enter Duration <br/> In Seconds
-            </label>
-          </div>
-          <div className="md:w-2/3">
-            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="number" min={0} max={60} placeholder='1-60'/>
-          </div>
-        </div>
-
-        {isListening ? <span>🎙️  Recording</span> : <span>🛑🎙️ Currently Not Recording</span>}
-
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <button id = "record" onClick={() => {setIsListening(prevState => !prevState)}} className="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="button">
-              Record
-            </button>
-          </div>
-        </div>
-      </form>
-
-    <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <audio controls className="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="audio">
-            </audio>
-          </div>
-    </div>
-
-    
-    </>
-
-  );
 }
