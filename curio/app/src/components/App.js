b9896b51490819407d11d5ed4eee8096d5b84e80
Arjxn.py <arjxn.py@gmail.com>
2022-09-04 01:28:01 +0530

    Added Predefined Duration & Auto Terminate Functionality (#7)
    * Recorder UI Added
    
    * Update curio/app/src/components/App.js
    
    Co-authored-by: Saransh <saransh0701@gmail.com>
    
    * Added Recorder Function Alongwith Speech-to-text
    
    * Player UI Added
    
    * Added Functional Components For Recorder
    
    * Added Predefined Duration and Auto Terminate Funcionality
    
    Co-authored-by: Saransh <saransh0701@gmail.com>

--- curio/app/src/components/App.js
+++ curio/app/src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import { Recorder , myRecorder} from 'react-voice-recorder';
 // import ReactPlayer from 'react-player/youtube'
-import {Form} from './recorder'
+import Audio from './recorder'
 import './App.css'
 
 function App () {
   return(
     <div>
-    {Form()}
+    <Audio/>
     </div>
     
   )
--- curio/app/src/components/recorder.js
+++ curio/app/src/components/recorder.js
@@ -47,19 +47,32 @@ export default class Audio extends Component {
           this.setState({ isRecording: true });
         }).catch((e) => console.error(e));
     }
+    let duration = document.querySelector('input').value;
+    setTimeout(this.stop , duration * 1000)
   }
 
   stop() {
     Mp3Recorder
       .stop()
       .getMp3()
-      .then(([buffer, blob]) => {
-        const blobURL = URL.createObjectURL(blob)
+
+      .then(([buffer , blob])=>{
+        const blobURL = URL.createObjectURL(blob);
+
+        let dataArray = [];
+        dataArray.push(blobURL);
+        console.log(dataArray);
+
         this.setState({ blobURL, isRecording: false });
         this.setState({ isRecordingStp: true });
-      }).catch((e) => console.log(e));
+        }).catch((e) => console.log(e));
+
   };
 
+  getDuration () {
+    let duration = document.querySelector('input').value;
+    console.log(duration);
+  }
   reset() {
       document.getElementsByTagName('audio')[0].src = '';
       this.setState({ isRecordingStp: false });
@@ -79,8 +92,15 @@ export default class Audio extends Component {
            </div>
            <div className="md:w-2/3">
              <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="number" min={0} max={60} placeholder='1-60'/>
+
+             <button id = "record" onClick={this.getDuration} className="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="button">
+               Set Timer
+             </button>
            </div>
          </div>
+
+        
+         {this.isRecording ? <span>🛑🎙️  Not Recording</span> : <span>🎙️ Click Record to Start Recording</span>}
 
          <div className="md:flex md:items-center">
            <div className="md:w-1/3"></div>
@@ -88,9 +108,11 @@ export default class Audio extends Component {
              <button id = "record" onClick={this.start} disabled={this.state.isRecording} className="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="button">
                Record
              </button>
-             <button id = "stop" onClick={this.stop} disabled={!this.state.isRecording} className="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="button">
+
+             {/* <button id = "stop" onClick={this.stop} disabled={!this.state.isRecording} className="shadow bg-purple-500 hover:bg-purple-400 hover:text-white focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded" type="button">
                Stop
-             </button>
+             </button> */}
+
            </div>
          </div>
 
