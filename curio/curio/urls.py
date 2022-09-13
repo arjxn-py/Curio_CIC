efcb60d70835836f2e5bd9c6ad777c45aba306bc
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-09-14 02:04:22 +0530

    Configured Superuser

--- curio/curio/urls.py
+++ curio/curio/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path , include
 from . import views
 # from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
     # path('djcurio/',include('djcurio.urls')),
-    # path('admin/', admin.site.urls),
     # path('',TemplateView.as_view(template_name = 'index.html')),
 ]
