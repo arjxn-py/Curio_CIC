f8ad87c406992b31b039ca0a406bd7fd08fe88c3
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-09-16 03:32:08 +0530

    MultiPages Connected

---
+++ curio/djcurio/views.py
@@ -0,0 +1,22 @@
+import requests
+
+from django.conf import settings
+from django.shortcuts import render
+
+def index(request):
+    search_url = 'https://www.googleapis.com/youtube/v3/search'
+
+    params = {
+        'part' : 'snippet',
+        'q' : 'python',
+        'key' : settings.YOUTUBE_DATA_API_KEY
+    }
+
+    r = requests.get(search_url , params=params)
+    print(r.text)
+
+    return render(request, 'index.html')
+
+
+def translate(request):
+    return render(request, 'translate.html')
