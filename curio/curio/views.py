7d0b801f4f3a971863a176035d0b650a0a85f7f5
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-09-18 00:14:00 +0530

    Video Object Created

--- curio/curio/views.py
+++ curio/curio/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import render
 
 def index(request):
     search_url = 'https://www.googleapis.com/youtube/v3/search'
+    video_url = 'https://www.googleapis.com/youtube/v3/videos'
 
-    params = {
+    search_params = {
         'part' : 'snippet',
         'q' : 'python',
         'key' : settings.YOUTUBE_DATA_API_KEY,
@@ -14,11 +15,29 @@ def index(request):
         'type' : 'video'
     }
 
-    r = requests.get(search_url , params=params)
+    video_ids = []
+
+    r = requests.get(search_url , params=search_params)
 
     results = r.json()['items']
 
     for result in results:
-        print(result['id']['videoId'])
+        video_ids.append(result['id']['videoId'])
+
+    video_params = {
+        'key' : settings.YOUTUBE_DATA_API_KEY,
+        'part' : 'snippet , contentDetails',
+        'id' : ','.join(video_ids)
+    }
+
+    v = requests.get(video_url,params=video_params)
+    vid_results = v.json()['items']
+
+    for vid_result in vid_results:
+        print(vid_result['snippet']['title'])
+        print(vid_result['id'])
+        print(vid_result['contentDetails']['duration'])
+        print(vid_result['snippet']['thumbnails']['high']['url'])
+
 
     return render(request, 'index.html')
